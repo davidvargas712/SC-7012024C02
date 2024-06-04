f5e83c7098656a6bd5261266063cacb6692f7563
davidvargas712 <vargasdavid035@gmail.com>
2024-06-04 15:26:26 -0600

    creacion de pokemon abstraccion

---
+++ Pokemon/Abstracciones/Modelos/Pokemon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracciones.Modelos
+{
+    public class Pokemon
+    {
+        public Guid id { get; set; }
+
+        public int Numero{ get; set; }
+
+        public int Nivel { get; set; }
+    }
+}
